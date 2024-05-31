2769b081f9fe9573ed06cd13ad09cacfaf0b71d8
RozovrRD <33757206+RozovrRD@users.noreply.github.com>
2024-05-31 13:42:31 +0300

    Update main.py

--- task1/main.py
+++ task1/main.py
@@ -3,21 +3,39 @@ import json
 import sys
 # task1
 
-n = int(sys.argv[1])
-m = int(sys.argv[2])
-iter = 1
-lengh = m - 1
-path = [str(iter)]
-
-while True:
-    if iter == n:
-        iter = 0
-    iter += 1
-    lengh -= 1
-    if iter == 1 and lengh == 0:
-        break
-    if lengh == 0:
+n = 5
+m = -4
+if n < 1:
+    print('numbers from 1 to n means n is greater than 1, so.. enter positive n please')
+else:
+    if m > 0:
+        iter = 1
         lengh = m - 1
-        path.append(str(iter))
-
-print(''.join(path))
+        path = [str(iter)]
+        
+        while True:
+            if iter == n:
+                iter = 0
+            iter += 1
+            lengh -= 1
+            if iter == 1 and lengh == 0:
+                break
+            if lengh == 0:
+                lengh = m - 1
+                path.append(str(iter))
+    else:
+        iter = 1
+        lengh = abs(m) - 1
+        path = [str(iter)]
+        
+        while True:
+            if iter == 1:
+                iter = iter + n
+            iter -= 1
+            lengh -= 1
+            if iter == 1 and lengh == 0:
+                break
+            if lengh == 0:
+                lengh = abs(m) - 1
+                path.append(str(iter))
+    print(''.join(path))
